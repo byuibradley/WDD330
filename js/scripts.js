4a229aad3ea75fd9a3ec622508f96ba2570aea6c
byuibradley <btrede@byui.edu>
2022-10-03 10:37:50 -0600

    updates for week 1

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+ const links = [
+  {
+    label: "Week01 Notes",
+    url: "week01/index.html"
+}, {
+    label: "Week02 Notes",
+    url: "week02/index.html"
+}
+  ];
+  
+  function createLinks() {
+    links.forEach(link => {
+        let listItem = document.createElement("li");
+        listItem.innerHTML = `<a href="${link.url}">${link.label}</a>`;
+        document.getElementById("links").appendChild(listItem);
+    });
+}; 
+
+
